e09970df6b68495f190e8dfacde4e0f1e1633ffb
adrian <anlumbad@gmail.com>
2021-06-14 09:03:30 +0800

    ready for heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const ejs        = require('ejs');
 const Router     = require('./routes'); 
 const bodyParser = require('body-parser');
 
-const server  = app.listen(3000, function(){
+const server  = app.listen(process.env.PORT || 3000, function(){
     console.log('listenign at 3000!')
 });
 const io      = require('socket.io')(server);
